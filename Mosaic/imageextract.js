063b3c27145b75aceb4162c9a8f104c8e670f67b
chizaram92 <c.chizaram@yahoo.com>
2020-03-05 17:03:25 -0600

    added background details

---
+++ Mosaic/imageextract.js
@@ -0,0 +1,14 @@
+//function to dynamically update and place image in an array after placed in file
+
+//upload image 
+
+	
+	//upload image 
+	function upload_image(image){
+		
+		console.log('successfully uploaded');
+	}
+	//put into array
+	function update_array(){
+		
+	}
